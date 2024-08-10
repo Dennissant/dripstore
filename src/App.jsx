62047b3e55d4f6c116cc16a5379251139ef5f1be
dennis <dennysedu50@gmail.com>
2024-08-10 11:44:57 -0300

    conf pra github page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { BrowserRouter} from 'react-router-dom'
-import Rotas from './pages/Rotas'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import Rotas from './pages/Rotas';
 
+// Detecta se o ambiente é de produção ou desenvolvimento
+const isProduction = process.env.NODE_ENV === 'production';
+const basename = isProduction ? '/dripstore' : '/';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<BrowserRouter basename="/dripstore">
-   <Rotas></Rotas>
-</BrowserRouter>
-)
+  <BrowserRouter basename={basename}>
+    <Rotas />
+  </BrowserRouter>
+);
